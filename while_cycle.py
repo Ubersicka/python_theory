eace94b3a70268d3deeffe31429bd04eeba93c42
Metodi Angelov <101995681+Ubersicka@users.noreply.github.com>
2022-03-20 23:51:00 +0200

    Create while_cycle.py

---
+++ while_cycle.py
@@ -0,0 +1,21 @@
+# a = 5
+#
+# while a <= 10:                   #Отброява числата до 10, защото след 10 вече е 11, а в условието е ясно, да не превишава 10
+#     print("a = " + str(a))     #Контетинация се ползва, превръщам те в str за да мога да контетенирам.
+#     a += 1
+
+# command = input()
+# while command != "Stop":   # ще е безкраен цикъл ако не е в цикала да има това на 11 ред command = input()
+#     print(command)
+#
+#     command = input()
+
+# for number in range(1,10):
+#     pass
+# while number < 10:
+#     break #break работи само в тялото на тялото
+# number = 5
+# while True:
+#     print(number)
+#     continue #като стигне до тук се връща обратно в началото на цикъла.
+#     print("alacsjbasfjtex")
